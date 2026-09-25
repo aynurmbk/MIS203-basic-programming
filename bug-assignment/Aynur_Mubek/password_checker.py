5b6bd1d89dd8c46a4bfb5686f78a6b76e7f886fb
aynurmbk <aynurrr.mbk1011@gmail.com>
2026-09-25 23:57:00 +0300

    Update password_checker.py

--- bug-assignment/Aynur_Mubek/password_checker.py
+++ bug-assignment/Aynur_Mubek/password_checker.py
@@ -1,9 +1,9 @@
 correct_password = "basic203"
 attempts = 0
-while True
+while True:
     password = input("Enter password: ")
-    attempts = attempts + "1"
-    if password != correct_password:
+    attempts = attempts + 1
+    if password == correct_password:
         print("Access granted!")
         break
     if attempts == 3:
